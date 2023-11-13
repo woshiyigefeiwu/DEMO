947763bc2833bd1736369bee1f13dc284e989de8
fjbq <2698043112@qq.com>
2023-11-13 16:39:16 +0800

    remove

--- Demo_1/Source/Demo_1/Demo_1.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Demo_1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Demo_1, "Demo_1" );
- 
